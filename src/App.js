6c958607e1c0ffea0d1c14bd0712285a4383a48b
Filimone Leaaetoa <filimone.leaaetoa@gmail.com>
2021-05-30 21:04:48 +1000

    added all required redux logic in userSlice.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
+import { auth, provider } from "./firebase";
+import { usedispatch, useSelector } from "react-redux";
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        
       </header>
     </div>
   );
--- src/features/userSlice.js
+++ src/features/userSlice.js
@@ -1,12 +1,35 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {};
+// const [userName, setUserName] = userState(null)
+// const [userEmail, setUserEmail] = userState(null)
 
+const initialState = {
+  userName: null,
+  userEmail: null,
+};
+
+// We define how we want setUserName and setuserEmail to function with userSlice
 const userSlice = createSlice({
-  name: user,
+  name: "user",
   initialState,
-  reducers: {},
+  reducers: {
+    setActiveUser: (state, action) => {
+      state.userName = action.payload.userName; // setUserName()
+      state.userEmail = action.payload.userEmail; //setuserEmail()
+    },
+    // When user logs out username and email will be set to null
+    setUserLogOutState: (state) => {
+      state.userName = null;
+      state.userEmail = null;
+    },
+  },
 });
 
-export const {} = userSlice.actions;
+// export setter functions
+export const { setActiveUser, setUserLogOutState } = userSlice.actions;
+
+// export getter variabels
+export const selectUsername = (state) => state.user.userName;
+export const selectUserEmail = (state) => state.user.userEmail;
+
 export default userSlice.reducer;
